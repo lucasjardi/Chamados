2da20eaf4e6c3e92d646229d651545a59154f4a8
Lucas Jardim <lcsjardim@gmail.com>
2017-10-31 19:47:49 -0200

    Add files via upload

---
+++ src/aberturachamados/AberturaChamados.java
@@ -0,0 +1,26 @@
+package aberturachamados;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AberturaChamados extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/login/login.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/view/assets/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
